ad91ece0357bb27b64d85e04124de4b3d0038b72
liuyazhou <leitingtod@126.com>
2018-01-16 16:32:30 +0800

    reformat helm-install-*

--- manifests/helm-install-grafana.sh
+++ manifests/helm-install-grafana.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
-helm install --name grafana stable/grafana --set server.service.type=NodePort --set rbac.create=true --namespace kube-system --set server.persistentVolume.enabled=false
+helm install --name grafana stable/grafana \
+    --namespace kube-system \
+    --set server.service.type=NodePort \
+    --set rbac.create=true \
+    --set server.persistentVolume.enabled=false
+
--- manifests/helm-install-kube-dashboard.sh
+++ manifests/helm-install-kube-dashboard.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
-helm install -n kubernetes-dashboard stable/kubernetes-dashboard --set ingress.enabled=true --set rbac.create=true --set ingress.hosts[0]=kubernetes-dashboard.aysaas.com --set imageTag=v1.8.1 --namespace kube-system
+helm install -n kubernetes-dashboard stable/kubernetes-dashboard \
+    --namespace kube-system \
+    --set ingress.enabled=true \
+    --set rbac.create=true \
+    --set ingress.hosts[0]=kubernetes-dashboard.aysaas.com \
+    --set imageTag=v1.8.1
 
