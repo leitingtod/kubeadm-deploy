993b307720e6a4aa3ebdd2c39f49c6ba71ca0acf
liuyazhou <leitingtod@126.com>
2018-01-24 13:59:35 +0800

    dynamic provisioner is great, but should use exist pvc if you want to use the same pv

--- manifests/ci.cd/jenkins/helm-install-jenkins.sh
+++ manifests/ci.cd/jenkins/helm-install-jenkins.sh
@@ -3,9 +3,15 @@
 helm install -n jenkins stable/jenkins \
     --set rbac.install=true \
     --set Persistence.Enabled=true \
-    --set Persistence.ExistingClaim="jenkins-nfs-pvc001" \
+    --set Persistence.ExistingClaim="jenkins-jenkins" \
+    --set Master.ImageTag="lts-slim" \
     --set Master.ServiceType=ClusterIP \
     --set Master.InstallPlugins="" \
     --set Master.HostName="jenkins.aysaas.com"
 
+#    --set Persistence.Annotations="volume.beta.kubernetes.io/storage-class: aysaas-nfs" \
+#    --set Persistence.StorageClass="aysaas-nfs" \
+
     #--set Master.Ingress.Annotations{="kubernetes.io/ingress.class":"nginx"}
+
+#     --set Agent.ImageTag="" \
