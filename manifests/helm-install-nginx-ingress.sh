b0964638ad09ef6235d549c6943fdca721e92429
liuyazhou <leitingtod@126.com>
2018-01-26 17:15:40 +0800

    完成执行 kubeadm的准备工作

--- manifests/helm-install-nginx-ingress.sh
+++ manifests/helm-install-nginx-ingress.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 helm install --name nginx-ingress stable/nginx-ingress \
     --set rbac.create=true \
-    --set controller.service.type=NodePort
+    --set controller.service.type=NodePort \
+    --set controller.service.nodePorts.http=80 \
+    --set controller.service.nodePorts.https=443
