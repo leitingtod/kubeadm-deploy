3d86497d360159dd8b4dd129afc74a2c7bd0cf9e
liuyazhou <leitingtod@126.com>
2018-01-30 16:34:09 +0800

    update heapster/grafana/influxdb to new release add del to install scripts

--- manifests/helm-install-nginx-ingress.sh
+++ manifests/helm-install-nginx-ingress.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
-helm install --name nginx-ingress stable/nginx-ingress \
-    --namespace kube-system \
-    --set rbac.create=true \
-    --set controller.service.type=NodePort \
-    --set controller.service.nodePorts.http=80 \
-    --set controller.service.nodePorts.https=443 \
-    --set controller.image.repository=dockerhub.aysaas.com/kubernetes/nginx-ingress-controller \
-    --set controller.image.tag=0.9.0-beta.15 \
-    --set defaultBackend.image.repository=dockerhub.aysaas.com/kubernetes/defaultbackend \
-    --set defaultBackend.image.tag=1.4
+
+NAME=nginx-ingress
+
+if [ ${1}x == "delx" ]; then
+    helm del --purge ${NAME}
+else
+    helm install --name ${NAME} stable/nginx-ingress \
+        --namespace kube-system \
+        --set rbac.create=true \
+        --set controller.service.type=NodePort \
+        --set controller.service.nodePorts.http=80 \
+        --set controller.service.nodePorts.https=443 \
+        --set controller.image.repository=dockerhub.aysaas.com/kubernetes/nginx-ingress-controller \
+        --set controller.image.tag=0.9.0-beta.15 \
+        --set defaultBackend.image.repository=dockerhub.aysaas.com/kubernetes/defaultbackend \
+        --set defaultBackend.image.tag=1.4
+fi
