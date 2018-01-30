f2edc080eaf8f557d66b6a33391354c54b780caa
liuyazhou <leitingtod@126.com>
2018-01-30 13:06:31 +0800

    update nginx-ingress to 0.9.0-bata.15, defaultbackend to 1.4

--- manifests/helm-install-nginx-ingress.sh
+++ manifests/helm-install-nginx-ingress.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 helm install --name nginx-ingress stable/nginx-ingress \
+    --namespace kube-system \
     --set rbac.create=true \
     --set controller.service.type=NodePort \
     --set controller.service.nodePorts.http=80 \
-    --set controller.service.nodePorts.https=443
+    --set controller.service.nodePorts.https=443 \
+    --set controller.image.repository=dockerhub.aysaas.com/kubernetes/nginx-ingress-controller \
+    --set controller.image.tag=0.9.0-beta.15 \
+    --set defaultBackend.image.repository=dockerhub.aysaas.com/kubernetes/defaultbackend \
+    --set defaultBackend.image.tag=1.4
