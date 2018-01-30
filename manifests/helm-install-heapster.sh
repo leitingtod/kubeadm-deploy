52a9b2d1945deec2344d51d4563d16a758502e77
liuyazhou <leitingtod@126.com>
2018-01-30 17:37:14 +0800

    add nfs-client installation and alias kk

--- manifests/helm-install-heapster.sh
+++ manifests/helm-install-heapster.sh
@@ -8,9 +8,9 @@ else
     helm install --name ${NAME} stable/heapster \
         --namespace kube-system \
         --set rbac.create=true \
-        --set image.repository=dockerhub.aysaas.com/kubernetes/heapster-amd64 \
-        --set image.tag=v1.5.0 \
+        --set image.repository=dockerhub.aysaas.com/kubernetes/heapster \
+        --set image.tag=v1.3.0 \
         --set resizer.image.repository=dockerhub.aysaas.com/kubernetes/addon-resizer \
-        --set resizer.image.tag=1.8.1 \
+        --set resizer.image.tag=1.7 \
         --set eventer.enabled=true
 fi
