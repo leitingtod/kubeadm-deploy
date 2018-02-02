becf34fa92c09545a2e1f9946f1c77f99a9f10a2
liuyazhou <leitingtod@126.com>
2018-02-02 15:59:22 +0800

    update jenkins to jenkins/jenkins:lts-alpine

--- manifests/ci.cd/jenkins/helm-install-jenkins.sh
+++ manifests/ci.cd/jenkins/helm-install-jenkins.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
 
-helm install -n jenkins stable/jenkins \
-    --set rbac.install=true \
-    --set Persistence.Enabled=true \
-    --set Persistence.ExistingClaim="jenkins-jenkins" \
-    --set Master.ImageTag="lts-slim" \
-    --set Master.ServiceType=ClusterIP \
-    --set Master.InstallPlugins="" \
-    --set Master.HostName="jenkins.aysaas.com"
+NAME=jenkins
+
+if [ ${1}x == "delx" ]; then
+    helm del --purge ${NAME}
+else
+    helm install -n jenkins stable/jenkins \
+        --set rbac.install=true \
+        --set Persistence.Enabled=true \
+        --set Persistence.ExistingClaim="jenkins-jenkins" \
+        --set Master.Image="dockerhub.aysaas.com/kubernetes/jenkins/jenkins" \
+        --set Master.ImageTag="lts-alpine" \
+        --set Master.ServiceType=ClusterIP \
+        --set Master.InstallPlugins="" \
+        --set Master.HostName="jenkins.az1.aysaas.com"
+fi
 
 #    --set Persistence.Annotations="volume.beta.kubernetes.io/storage-class: aysaas-nfs" \
 #    --set Persistence.StorageClass="aysaas-nfs" \
