5ce295a874f6b6a86068e4c74b15c68d290b386a
liuyazhou <leitingtod@126.com>
2018-02-27 10:17:23 +0800

    update helm kube-dashboard to 0.5.3

--- manifests/helm-install-kube-dashboard.sh
+++ manifests/helm-install-kube-dashboard.sh
@@ -9,7 +9,7 @@ else
         --namespace kube-system \
         --set ingress.enabled=true \
         --set rbac.create=true \
-        --set ingress.hosts[0]=kubernetes-dashboard.az1.aysaas.com \
-        --set image=dockerhub.aysaas.com/kubernetes/kubernetes-dashboard-amd64 \
-        --set imageTag=v1.8.2
+        --set ingress.hosts[0]=kubernetes-dashboard.az3.aysaas.com \
+        --set image.repository=dockerhub.aysaas.com/kubernetes/kubernetes-dashboard-amd64 \
+        --set image.tag=v1.8.2
 fi
