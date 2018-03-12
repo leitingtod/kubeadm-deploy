d5ce0bfecfbe176e2cb7f9ae13c2b2a3eba4eaa0
liuyazhou <leitingtod@126.com>
2018-03-12 14:06:18 +0800

    增加指定域名的功能

--- manifests/helm-install-kube-dashboard.sh
+++ manifests/helm-install-kube-dashboard.sh
@@ -5,11 +5,15 @@ NAME=kubernetes-dashboard
 if [ ${1}x == "delx" ]; then
     helm del --purge ${NAME}
 else
+    if [ $1 == "" ]; then
+        echo "请指定域名！域名建议：kube-dashboard.az[3].aysaas.com"
+    fi
+
     helm install -n ${NAME} stable/kubernetes-dashboard \
         --namespace kube-system \
         --set ingress.enabled=true \
         --set rbac.create=true \
-        --set ingress.hosts[0]=kubernetes-dashboard.az3.aysaas.com \
+        --set ingress.hosts[0]=${domain} \
         --set image.repository=dockerhub.aysaas.com/kubernetes/kubernetes-dashboard-amd64 \
         --set image.tag=v1.8.2
 fi
