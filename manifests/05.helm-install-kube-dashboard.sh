e040e670a1d14c6c7b7b1aae28ac4e6d1051d7a6
liuyazhou <leitingtod@126.com>
2018-04-08 14:55:50 +0800

    update to chart 0.5.3

--- manifests/05.helm-install-kube-dashboard.sh
+++ manifests/05.helm-install-kube-dashboard.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 
-# version: 0.4.1
-# appVersion: 1.7.1
+# version: 0.5.3
+# appVersion: 1.8.3
 
 NAME=kubernetes-dashboard
 
 
-if [ ${1}x == "delx" ]; then
+if [ "${1}" = "del" ]; then
     helm del --purge ${NAME}
 else
-    if [ $1 == "" ]; then
-        echo "请指定域名！建议域名：kube-dashboard.az[数字].aysaas.com"
+    if [ "$1" = "" ]; then
+        echo "请指定域名！建议域名：kube-dashboard.az[Number].aysaas.com"
+        exit 1
     fi
 
-    helm install -n ${NAME} aysaas/kubernetes-dashboard --version 0.4.1 \
+    helm install -n ${NAME} stable/kubernetes-dashboard --version 0.5.3 \
         --namespace kube-system \
-        --set ingress.enabled=true \
         --set rbac.create=true \
+        --set ingress.enabled=true \
         --set ingress.hosts[0]=${1} \
-        --set image=dockerhub.aysaas.com/kubernetes/kubernetes-dashboard-amd64 \
-        --set imageTag=v1.8.3
+        --set image.repository=dockerhub.aysaas.com/kubernetes/kubernetes-dashboard-amd64 \
+        --set image.tag=v1.8.3
 fi
-
