64f49cbdf0f37320a5ac10173affe2ec6725e85a
liuyazhou <leitingtod@126.com>
2018-04-08 15:17:04 +0800

    update heapster, grafana, influxdb to latest

--- manifests/50.helm-install-heapster.sh
+++ manifests/50.helm-install-heapster.sh
@@ -11,9 +11,10 @@ else
     helm install --name ${NAME} stable/heapster \
         --namespace kube-system \
         --set rbac.create=true \
-        --set image.repository=dockerhub.aysaas.com/kubernetes/heapster \
-        --set image.tag=v1.3.0 \
-        --set resizer.image.repository=dockerhub.aysaas.com/kubernetes/addon-resizer \
-        --set resizer.image.tag=1.7 \
         --set eventer.enabled=true
 fi
+
+#        --set image.repository=dockerhub.aysaas.com/kubernetes/heapster \
+#        --set image.tag=v1.3.0 \
+#        --set resizer.image.repository=dockerhub.aysaas.com/kubernetes/addon-resizer \
+#        --set resizer.image.tag=1.7 \
