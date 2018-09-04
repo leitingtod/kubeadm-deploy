018f0eea4268ac3597ce53b6c2b29deff05d0438
liuyazhou <leitingtod@126.com>
2018-09-04 10:37:25 +0800

    support upgrade operation

--- manifests/05.helm-install-kube-dashboard.sh
+++ manifests/05.helm-install-kube-dashboard.sh
@@ -5,29 +5,45 @@
 
 NAME=kubernetes-dashboard
 
-if [ "${1}" = "del" ]; then
-	helm del --purge ${NAME}
-else
-	if [ "$1" = "" ]; then
-		echo "请指定域名！建议域名：kube-dashboard.az[Number].aysaas.com"
-		exit 1
-	fi
+values_file=/tmp/values.yml
 
-	values_file=/tmp/values.yml
+case "$1" in
+    del | upgrade | install) ;;
+    *)
+        echo "Error: only support subcommand install | upgrade | del !"
+        exit 1
+        ;;
+esac
 
-	echo "
-rabc:
+if [ "$2" = "" ]; then
+    echo "请指定域名！建议域名：kube-dashboard.az[Number].aysaas.com"
+    exit 1
+fi
+
+echo "
+rbac:
   create: true
 ingress:
   enabled: true
   hosts:
-    - ${1}
+    - ${2}
 image:
-  repository: dockerhub.aysaas.com/kubernetes/kubernetes-dashboard-amd64
-  tag: v1.8.3
+  repository: anjia0532/google-containers.kubernetes-dashboard-amd64
+  tag: v1.10.0
 " >${values_file}
 
-	helm install -n ${NAME} stable/kubernetes-dashboard --version 0.5.3 --namespace kube-system -f ${values_file}
-
-	kubectl patch -n kube-system deploy ${NAME} -p "$(cat ./tolerations.json)"
-fi
+case "$1" in
+    del)
+        helm del --purge ${NAME}
+        ;;
+    upgrade)
+        helm upgrade ${NAME} stable/kubernetes-dashboard --version 0.5.3 --namespace kube-system -f ${values_file}
+        ;;
+    install)
+        helm install -n ${NAME} stable/kubernetes-dashboard --version 0.5.3 --namespace kube-system -f ${values_file}
+        kubectl patch -n kube-system deploy ${NAME} -p "$(cat ./tolerations.json)"
+        ;;
+    *)
+        echo "Error: only support subcommand install | upgrade | del !"
+        ;;
+esac
